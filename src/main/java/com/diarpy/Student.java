c678e0350dc871d7ae03657fabdbab1d9e03a2a0
Rutzno <d.rutzno@gmail.com>
2022-07-29 12:32:05 +0000

    stage 3

---
+++ src/main/java/com/diarpy/Student.java
@@ -0,0 +1,88 @@
+package tracker;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class Student {
+    private int ID;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Map<String, Integer> courses = new LinkedHashMap<>();
+    private static int numStudents;
+
+    static {
+        numStudents = 9999;
+    }
+
+    public Student(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        ID = ++numStudents;
+    }
+
+    public boolean grade(int... notes) {
+        try {
+            if (notes.length != 4 || notes[0] < 0 || notes[1] < 0 || notes[2] < 0 || notes[3] < 0) {
+                System.out.println("Incorrect points format.");
+                return false;
+            }
+           /* for (int n : notes) {
+                if (n < 0) {
+                    System.out.println("Incorrect points format.");
+                    return;
+                }
+            }*/
+            courses.put("Java", courses.getOrDefault("Java", 0) + notes[0]);
+            courses.put("DSA", courses.getOrDefault("DSA", 0) + notes[1]);
+            courses.put("Databases", courses.getOrDefault("Databases", 0) + notes[2]);
+            courses.put("Spring", courses.getOrDefault("Spring", 0) + notes[3]);
+            return true;
+
+        } catch (Exception e) {
+            System.out.println("Incorrect points format.");
+            return false;
+        }
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Map<String, Integer> getCourses() {
+        return courses;
+    }
+
+    public static int getNumStudents() {
+        return numStudents;
+    }
+}
