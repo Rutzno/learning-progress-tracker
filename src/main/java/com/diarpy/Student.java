ebe9be8773abcad674f8fb504e30b9fc1f201686
Rutzno <d.rutzno@gmail.com>
2022-07-29 12:47:36 +0000

    stage 4

--- src/main/java/com/diarpy/Student.java
+++ src/main/java/com/diarpy/Student.java
@@ -3,6 +3,12 @@ package com.diarpy;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+/**
+ * @author Mack_TB
+ * @since 24/06/2022
+ * @version 1.0.4
+ */
+
 public class Student {
     private int ID;
     private String firstName;
